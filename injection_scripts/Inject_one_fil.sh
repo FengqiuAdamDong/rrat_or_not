1a10f5109fdb62b7d7c25c40bdc14432056fdda9
adamdong <adamdong@phas.ubc.ca>
2022-07-26 10:25:13 -0700

    changed time

--- injection_scripts/Inject_one_fil.sh
+++ injection_scripts/Inject_one_fil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rrg-istairs-ad
 #SBATCH --export=NONE
-#SBATCH --time=20:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem=24GB
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=injections
