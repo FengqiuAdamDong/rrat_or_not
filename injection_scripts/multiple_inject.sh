7e87922f46de63be146cb075637ade691d7dcb32
adamdong <adamdong@phas.ubc.ca>
2023-03-17 17:16:51 -0700

    loading modules before running python

--- injection_scripts/multiple_inject.sh
+++ injection_scripts/multiple_inject.sh
@@ -19,6 +19,11 @@ do
     #copy the filterbank file back in
     cp -d $f $MASKFOL
     cd $MASKFOL
+    if [ "$LOCAL" != true ]; then
+        module use /project/6004902/modulefiles
+        module load presto
+        module load chime-psr
+    fi
     status=$(python $SOURCEDIR/detect_inject_status.py sample_injections.npz)
     if [ "$status" != 0 ]; then
         if [ "$LOCAL" != true ]; then
