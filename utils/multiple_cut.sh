d46bb24d726f2d1b849c3e79f51ab7c9eb3b2b09
adamdong <adamdong@phas.ubc.ca>
2023-04-26 13:22:44 -0700

    checks to see if file already cut

--- utils/multiple_cut.sh
+++ utils/multiple_cut.sh
@@ -3,5 +3,13 @@
 # This script will cut the filterbank files to the desired length
 for file in $@
 do
+    #strip the .fil extension
+    file=${file%.fil}
+    #check if the file is already cut
+    if [ -f ${file}_cut_80.fil ]
+    then
+        echo "File ${file}_cut.fil already exists, skipping"
+        continue
+    fi
     sbatch ~/rrat_or_not/utils/cut_single.sh 80 $file
 done
