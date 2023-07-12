2629a5c8e6490e6110d46148fc5444095fb8d730
adamdong <adamdong@phas.ubc.ca>
2023-07-12 10:50:40 -0700

    changing hours req and cpus per core

--- injection_scripts_fluence/detect_one_fil.sh
+++ injection_scripts_fluence/detect_one_fil.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=rrg-istairs-ad
 #SBATCH --export=NONE
-#SBATCH --time=10:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem=8GB
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --job-name=injections_det
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
