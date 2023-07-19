e68b6031860ee5707e278fa79bf4bb2ec69b5541
adamdong <adamdong@phas.ubc.ca>
2023-07-19 12:51:51 -0700

    bug fix

--- utils/bayes_vs_no_bayes.py
+++ utils/bayes_vs_no_bayes.py
@@ -220,6 +220,7 @@ if __name__=="__main__":
     filenames = args.filenames
     dp = dynesty_plot(filenames)
     dp.load_filenames()
+    dp.load_detections()
     snr_thresh = statistics_basic.load_detection_fn(det_curve,min_snr_cutoff=1.6)
 
     if args.exp:
