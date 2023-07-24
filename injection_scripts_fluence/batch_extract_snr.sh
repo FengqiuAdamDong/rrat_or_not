268895faef034cc02b0e816b099e052549abd355
adam <adamdong@phas.ubc.ca>
2023-07-24 09:45:50 -0700

    append to log

--- injection_scripts_fluence/batch_extract_snr.sh
+++ injection_scripts_fluence/batch_extract_snr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rrg-istairs-ad
 #SBATCH --export=NONE
-#SBATCH --time=30:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=extract_snr
@@ -10,4 +10,4 @@
 #
 source ~/util/load_presto.sh
 source ~/extract_snr/bin/activate
-python ~/rrat_or_not/injection_scripts_fluence/extract_snr.py -dm $1 -o $2 -ds 3 -p filtered_edit.csv
+python ~/rrat_or_not/injection_scripts_fluence/extract_snr.py -dm $1 -o $2 -ds 3 -p $3 >> extract_snr.log
