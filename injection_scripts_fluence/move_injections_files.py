3a8ad3aac7404e943f08ada117563e7ac873b116
adamdong <adamdong@phas.ubc.ca>
2023-07-26 08:47:38 -0700

    moving all the injections files to one level above

--- injection_scripts_fluence/move_injections_files.py
+++ injection_scripts_fluence/move_injections_files.py
@@ -15,16 +15,19 @@ if __name__=='__main__':
 
     files = os.listdir("./")
     files = [f for f in files if f.endswith(".fil")]
+    #get pwd
+    pwd = os.getcwd()
+    #go up one level so that we're creating everything in a directory above
+    pwd = os.path.join(pwd,"../")
+
 
     #check if path exists
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(os.path.join(pwd,path))
 
     #symbolic link every gap-th file to path
     for i in range(0,len(files),gap):
         active_file = files[i]
-        #get pwd
-        pwd = os.getcwd()
         os.symlink(os.path.join(pwd,active_file), os.path.join(pwd,os.path.join(path,active_file)))
         #basename
         basename = active_file.split(".")[0]
@@ -34,7 +37,7 @@ if __name__=='__main__':
         new_folder = os.path.join(pwd,os.path.join(path,basename))
         os.makedirs(new_folder)
         #list all rfifind files in the original folder
-        rfifind_files = glob.glob(os.path.join(pwd,os.path.join(basename,"*rfifind*")))
+        rfifind_files = glob.glob(os.path.join(os.getcwd(),os.path.join(basename,"*rfifind*")))
         #symbolic link all rfifind files to the new folder
         for rfifind_file in rfifind_files:
             print("symlinking "+rfifind_file+" to "+new_folder)
