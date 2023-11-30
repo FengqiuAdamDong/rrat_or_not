fixed some weird issue with detection of injected pulses, removed masking

injection_scripts_fluence/combine_inject_stats.py
@@ -31,6 +31,7 @@ class inject_stats_collection(inject_stats):
                 csv = f"{f}/positive_bursts_1.csv"
                 inst.get_base_fn()
                 inst.amplitude_statistics()
+                print(f"{f}")
                 inst.compare([csv], title=f)
                 self.det_snr.append(inst.det_snr)
                 self.detected_pulses.append(inst.detected_pulses)
@@ -62,16 +63,18 @@ class inject_stats_collection(inject_stats):
         # plt.scatter(self.detected_bin_midpoints,self.detected_det_frac,label=r"Measured $P(det|S_{det)$")
         plt.errorbar(self.detected_bin_midpoints,self.detected_det_frac,yerr=error_det_frac_d,fmt=".",label=r"Measured $P(det|S_{det})$")
         plt.legend()
-        plt.show()
-        plt.savefig("overall_detection_curve.png")
-        plt.close()
+        plt.xlim(0,5.5)
+        plt.savefig("det_frac_fit.pdf")
+        # plt.show()
+        # plt.savefig("overall_detection_curve.png")
+        # plt.close()
 
-        interp_p = np.interp(predict_x_array, self.detected_bin_midpoints, self.detected_det_frac)
-        plt.plot(predict_x_array, interp_p, label="interpolated")
-        plt.title("overall detection curve interp")
-        plt.legend()
-        plt.savefig("overall_detection_curve_interp.png")
-        plt.close()
+        # interp_p = np.interp(predict_x_array, self.detected_bin_midpoints, self.detected_det_frac)
+        # plt.plot(predict_x_array, interp_p, label="interpolated")
+        # plt.title("overall detection curve interp")
+        # plt.legend()
+        # plt.savefig("overall_detection_curve_interp.png")
+        # plt.close()
 
     def model(self,X, snr_fit, det_error, snr, det_frac, deg=5,plot=False):
         #create a model for the detection curve
@@ -85,14 +88,18 @@ class inject_stats_collection(inject_stats):
         interp_p = np.interp(snr, snr_fit, convolved)
         if plot:
             error_det_frac_i = np.sqrt(det_frac*(1-det_frac)/self.total_injections_per_snr)
-            plt.figure()
+            plt.figure(figsize=(8,5))
             plt.errorbar(snr,det_frac,yerr=error_det_frac_i,fmt=".",label=r"$P(det|S_T)$")
             # plt.scatter(snr,det_frac,label=r"$P(det|SNR_t)$")
-            plt.plot(snr,interp_p,label=r"Forward model $P(det|S_T)$")
             plt.plot(snr_fit,predict_y,label=r"Forward model $P(det|S_{det})$")
-            plt.xlabel("S")
+
+            plt.xlabel("S/N")
             plt.ylabel("Probability")
+            plt.axvline(2,linestyle="--",color="k")
+            plt.xlim(0,5.5)
             plt.legend()
+            plt.plot(snr,interp_p,label=r"Fitted $P(det|S_T)$")
+            plt.savefig("det_frac.pdf")
         return interp_p
 
     def loglikelihood(self, X, snr_fit, det_error, snr, det_frac, deg=10):
@@ -115,10 +122,12 @@ class inject_stats_collection(inject_stats):
         predict_y = self.predict_poly(snr_fit,x=snr,p=det_frac,poly=res.x)
         self.model(res.x,snr_fit,det_error,snr,det_frac,plot=True)
 
-        plt.figure()
+        plt.figure(figsize=(8,5))
         plt.plot(snr_fit,predict_y,label="Forward model $P(det|S_{det})$")
+        #add a line at x=2
+        plt.axvline(x=2,linestyle="--",color="black")
         # plt.scatter(snr,det_frac,label=r"$P(det|S_t)$")
-        plt.xlabel("SNR")
+        plt.xlabel("S/N")
         plt.ylabel("Probability")
         self.detected_snr_fit = snr_fit
         self.detected_det_frac_fit = predict_y

injection_scripts_fluence/inject_pulses_sigpyproc.py
@@ -19,10 +19,10 @@ from inject_stats import autofit_pulse
 # define the random number generator
 # np.random.seed(12345)
 
-# TRIAL_SNR = [10]
+TRIAL_SNR = [10]
 # 0.8,
 # ]  # total S/N of pulse
-TRIAL_SNR = np.linspace(0.1,6,20)
+# TRIAL_SNR = np.linspace(0.1,6,20)
 # TRIAL_SNR=[1,2,3,4,5,6,7,8]
 # TRIAL_SNR = [0.003]
 # TRIAL_SNR = np.linspace(0.1e-3, 5e-3, 50)
@@ -234,11 +234,14 @@ def inject_pulses(
         # SNR,amp, stats_std,loc,sigma_width =  calculate_SNR_wrapper(p,stats_window,tsamp,downsamp,data,masked_chans,plot)
         # print(f"fitted SNR: {SNR}, stats std: {stats_std}")
 
-    # data = data.astype("uint8")
-    # np.savez('data',data = data,masked_chans = masked_chans)
-    # for i, p in enumerate(pulse_attrs):
-        # statistics.append(calculate_SNR_wrapper(p,stats_window,tsamp,downsamp,copy.deepcopy(data),masked_chans,plot=True))
 
+    # data = data.astype("uint8")
+    np.savez('data',data = data,header=data.header,masked_chans = masked_chans,pulse_attrs=pulse_attrs,stats_window=stats_window,tsamp=tsamp,downsamp=downsamp)
+
+    for i, p in enumerate(pulse_attrs):
+        statistics.append(calculate_SNR_wrapper(p,stats_window,tsamp,downsamp,copy.deepcopy(data),masked_chans,plot=True))
+
+        # statistics.append(calculate_SNR_wrapper(p,stats_window,tsamp,1,copy.deepcopy(data),masked_chans,plot=True))
     return data, statistics
 
 
@@ -272,31 +275,32 @@ def calculate_SNR_wrapper(p,stats_window,tsamp,downsamp,data,masked_chans,plot):
         time_to_bin(p_toa + stats_window, tsamp),
     )
     #add 10 seconds extra time to the stats window
-    extra_time_bins = time_to_bin(5,tsamp)
+    extra_time_bins = time_to_bin(5-stats_window,tsamp)
     stats_start -= extra_time_bins
     stats_end += extra_time_bins
     #adjust so it's a multiple of downsamp
     stats_len = stats_end - stats_start
     stats_len = stats_len - stats_len%downsamp
     stats_end = stats_start + stats_len
-
     stats_data = copy.deepcopy(data[:,stats_start:stats_end])
     #dedisperse stats data
     stats_data = stats_data.dedisperse(p_dm)
     #downsample stats_data to the downsample that I will use for detection
     print(f"downmsampling with {downsamp}")
-
     stats_data = stats_data.downsample(tfactor=downsamp)
     stats_data = stats_data[~masked_chans,:]
     #get the window
-    focused_stats_start = time_to_bin(5,tsamp*downsamp)
-    focused_stats_end = time_to_bin(5+2*stats_window,tsamp*downsamp)
+    focused_stats_start = time_to_bin(5-stats_window,tsamp*downsamp)
+    focused_stats_end = time_to_bin(5-stats_window+2*stats_window,tsamp*downsamp)
     stats_data = stats_data[:, focused_stats_start:focused_stats_end]
     #fit a polynomial to the window
     #get the mean of the window
     stats_mean = np.mean(stats_data, axis=0)
-    amp,std,loc,sigma_width =  autofit_pulse(stats_mean,tsamp*downsamp,p_width*6,int(stats_window/tsamp/downsamp)
-                                                    ,data,downsamp,plot=plot)
+    plt.plot(stats_mean)
+    plt.show()
+    amp,std,loc,sigma_width =  autofit_pulse(stats_mean,tsamp*downsamp,0.1,int(stats_window/tsamp/downsamp)
+                                                    ,data,downsamp,plot=plot,fit_width_guess=p_width)
+    print(f"Masked channels {len(masked_chans)}: {sum(masked_chans)}")
     std = std * np.sqrt(sum(~masked_chans)/len(masked_chans))
     SNR = amp/std
     print(f"Inj SNR:{p_SNR} Det SNR: {SNR} std: {std} amp: {amp} loc: {loc} width: {sigma_width} inj amp: {p_SNR*std}")
@@ -340,36 +344,8 @@ def get_mask(rfimask, startsamp, N):
     return mask.T
 
 
-def get_pazi_mask(pazi_fn):
-    """
-    Loads a PAZI file using psrchive and returns a mask array that represents
-    the frequency and time bins that are not flagged in the data.
 
-    Args:
-    - pazi_fn: str, the filename of the PAZI file to load
-
-    Returns:
-    - masks: numpy.ndarray with shape (total_bins,), a 1D array that represents
-    the average mask across all subintegrations and channels in the PAZI file.
-    The values of the array are between 0 and 1, where 1 indicates that the
-    corresponding bin is not flagged (i.e., it should be included in data analysis),
-    and 0 indicates that the bin is flagged (i.e., it should be excluded).
-    """
-    import psrchive
-    arch = psrchive.Archive_load(pazi_fn)
-    #lets get the mask
-    total_subint = arch.get_nsubint()
-    total_chans = arch.get_nchan()
-    total_bins = arch.get_nbin()
-    mask_array = np.zeros((total_subint,1,total_chans,total_bins))
-    for i,integration in enumerate(arch):
-        for j in range(integration.get_nchan()):
-            mask_array[i,0,j,:] = integration.get_weight(j)
-    masks = np.mean(np.mean(np.mean(mask_array,axis=2),axis=1),axis=1)
-    return masks
-
-
-def get_filterbank_data_window(fn, maskfn, duration=20, pazi=False):
+def get_filterbank_data_window(fn, maskfn, duration=20):
     """Load a window of filterbank data from a .fil file, after applying a mask to discard some of the data.
 
     Parameters
@@ -400,55 +376,32 @@ def get_filterbank_data_window(fn, maskfn, duration=20, pazi=False):
     from sigpyproc import readers as r
     from sigpyproc.block import FilterbankBlock as fbb
     #find the archive filename
-    pazi_fn = fn.replace(".fil", "") + "_1000.00ms_Cand.pfd.pazi"
-    #load the archive
-    if pazi:
-        pazi_mask = get_pazi_mask(pazi_fn)
-    else:
-        pazi_mask = np.ones(1000)
     #load the weights
     print("getting filterbank data")
     filf = r.FilReader(fn)
     hdr = filf.header
     tsamp = hdr.tsamp
-    chunk_size = hdr.nsamples//len(pazi_mask)
-    chunk_dur = chunk_size * tsamp
-    #get required chunks
-    reuired_chunks = int(duration/chunk_dur)
     fil_dur = hdr.nsamples * tsamp
-    # start in the middle of the data
-    start_chunk = int(len(pazi_mask)//2 - reuired_chunks//2)
-    #recursively get the data and throw away if chunk is masked
-    acquired_chunks = 0
-    current_chunk = int(start_chunk)
-    channel_mask = np.zeros(hdr.nchans,dtype=bool)
-    while acquired_chunks < reuired_chunks:
-        #check if chunk is masked
-        if pazi_mask[current_chunk] == 0:
-            current_chunk += 1
-            continue
-        #get the data for this chunk
-        start_bin = current_chunk * chunk_size
-        _ = filf.read_block(start_bin, chunk_size)
-        masked_data, masked_chans = maskfile(maskfn, copy.deepcopy(_), start_bin, chunk_size)
-        #update the channel mask
-        channel_mask = np.logical_or(channel_mask,masked_chans)
-        #update the chunk counter
-        if acquired_chunks>0:
-            data = np.append(data,_,axis=1)
-        else:
-            data = _
-        acquired_chunks += 1
-        current_chunk += 1
+    #start in the middle of the data
+    start = fil_dur / 2 - duration / 2
+    stop = start + duration
+    print("start stop bins",start,stop)
+    start_bin = int(np.round(start / tsamp))
+    stop_bin = int(np.round(stop / tsamp))
+    nsamp = stop_bin-start_bin
+    # get the data
+    _ = filf.read_block(start_bin,nsamp)
+    masked_data, masked_chans = maskfile(maskfn, copy.deepcopy(_), start_bin, nsamp)
+
     # update the header so that it represents the windowed data
     presto_header = FilterbankFile(fn).header
-    hdr.tstart += (start_chunk * chunk_size * tsamp) / 86400.0  # add in MJD
-    hdr.nsamples = data.shape[1]  # total number of data samples (nchan * nspec)
-    hdr.nsamples_files = [data.shape[1]]
+    hdr.tstart += (start_bin * tsamp) / 86400.0  # add in MJD
+    hdr.nsamples = masked_data.shape[1]  # total number of data samples (nchan * nspec)
+    hdr.nsamples_files = [masked_data.shape[1]]
     hdr.tstart_files = [hdr.tstart]
-    data = fbb(data, hdr)
+    data = fbb(masked_data, hdr)
     #don't bother updating the presto header
-    return data, channel_mask, presto_header
+    return data, masked_chans, presto_header
 
 
 def multiprocess(arr):
@@ -559,17 +512,12 @@ if __name__ == "__main__":
         help="enable multiprocessing with 10 cores",
         action="store_true",
     )
-    parser.add_argument(
-        "--pazi",
-        action="store_true",
-        help="enable pazi masking",
-        )
+
     args = parser.parse_args()
 
     duration = args.d
     ifn = args.fil
     maskfn = args.m
-    pazi = args.pazi
     if args.injection_file:
         injection_sample = np.load(args.injection_file)
         npul = len(injection_sample)
@@ -614,7 +562,7 @@ if __name__ == "__main__":
                 p.map(multiprocess, pool_arr)
     else:
         rawdata, masked_chans, presto_header = get_filterbank_data_window(
-            ifn, duration=duration, maskfn=maskfn, pazi=pazi
+            ifn, duration=duration, maskfn=maskfn
         )
         for dm in TRIAL_DMS:
             pool_arr = []

injection_scripts_fluence/inject_stats.py
@@ -69,13 +69,15 @@ def maskfile(maskfn, data, start_bin, nbinsextra):
     mask = get_mask(rfimask, start_bin, nbinsextra)[::-1]
     print("get mask finished")
     masked_chans = mask.all(axis=1)
+    # masked_chans = np.load("data.npz")['masked_chans']
     # mask the data but set to the mean of the channel
-    mask_vals = np.median(data, axis=1)
-    for i in range(len(mask_vals)):
-        _ = data[i, :]
-        _m = mask[i, :]
-        _[_m] = mask_vals[i]
-        data[i, :] = _
+    # mask_vals = np.median(data, axis=1)
+    # for i in range(len(mask_vals)):
+    #     _ = data[i, :]
+    #     _m = mask[i, :]
+    #     _[_m] = mask_vals[i]
+    #     data[i, :] = _
+    print(f"Masked channels {len(masked_chans)}: {sum(masked_chans)}")
     return data, masked_chans
 
 
@@ -86,8 +88,10 @@ def extract_plot_data(data,masked_chans,dm,downsamp,nsamps_start_zoom,nsamps_end
     nsamps = waterfall_dat.shape[1]
     nsamps = nsamps - nsamps % downsamp
     waterfall_dat = waterfall_dat[:,0:nsamps]
+    waterfall_dat = waterfall_dat.dedisperse(dm)
     waterfall_dat = waterfall_dat.downsample(tfactor=downsamp)
-    dat_ts = np.mean(waterfall_dat[~masked_chans, :], axis=0)
+    dat_ts = copy.deepcopy(waterfall_dat)[~masked_chans, :]
+    dat_ts = np.mean(dat_ts, axis=0)
     dat_ts = dat_ts[int(nsamps_start_zoom / downsamp) : int(nsamps_end_zoom / downsamp)]
     #make sure that waterfall_dat is a multiple of 4
     nsamps = waterfall_dat.shape[1]
@@ -121,7 +125,6 @@ def grab_spectra_manual(
         t_start = t_start + ts*tsamp
         ts = 0
 
-
     print("start and end times", ts, te)
     nsamps = int((te - ts) / tsamp)
     nsamps = nsamps - nsamps % downsamp
@@ -130,7 +133,10 @@ def grab_spectra_manual(
     nsamps_start_zoom = int(t_start / tsamp)
     nsamps_end_zoom = int((t_dur+t_start) / tsamp)
     try:
+        import sigpyproc
         spec = g.read_block(ssamps, nsamps)
+        # spec = g.read_block(0, g.header.nsamples)
+        # spec = spec[:, ssamps : ssamps + nsamps]
     except Exception as e:
         print(e)
         import pdb; pdb.set_trace()
@@ -138,10 +144,9 @@ def grab_spectra_manual(
     # load mask
     if mask:
         print("masking data")
-        data, masked_chans = maskfile(mask_fn, spec, ssamps, nsamps)
-        print(sum(masked_chans))
-    data = data.dedisperse(dm)
+        data, masked_chans = maskfile(mask_fn, copy.deepcopy(spec), ssamps, nsamps)
     waterfall_dat, dat_ts = extract_plot_data(data,masked_chans,dm,downsamp,nsamps_start_zoom,nsamps_end_zoom)
+
 
     if manual:
         # this gives you the location of the peak
@@ -153,27 +158,6 @@ def grab_spectra_manual(
             ds_data=waterfall_dat,
             downsamp=downsamp,
         )
-
-#        while amp==-1:
-#            #fit has failed, get a larger time window and try again
-#            # make a copy to plot the waterfall
-#            t_start = t_start - 1
-#            t_dur = t_dur + 2
-#            print(t_start,t_dur)
-#            if (t_start<0)|((t_start+t_dur)>(te-ts)):
-#                break
-#            nsamps_start_zoom = int(t_start / tsamp)
-#            nsamps_end_zoom = int((t_dur+t_start) / tsamp)
-#            print(nsamps_start_zoom,nsamps_end_zoom)
-#            waterfall_dat, dat_ts = extract_plot_data(data,masked_chans,dm,downsamp,nsamps_start_zoom,nsamps_end_zoom)
-#            amp, std, loc, sigma_width = fit_SNR_manual(
-#                dat_ts,
-#                tsamp * downsamp,
-#                fit_del,
-#                nsamps=int(t_dur/2 / tsamp / downsamp),
-#                ds_data=waterfall_dat,
-#                downsamp=downsamp,
-#            )
         if (amp!=-1)&((loc<(0.49*t_dur))|(loc>(t_dur*0.51))|(sigma_width>2e-2)):
             #repeat if initial loc guess is wrong
             amp, std, loc, sigma_width = fit_SNR_manual(
@@ -193,21 +177,14 @@ def grab_spectra_manual(
             ts_no_ds_zoom_end = int(loc/tsamp + 0.9/tsamp)
             ts_no_ds = data[:, ts_no_ds_zoom_start : ts_no_ds_zoom_end]
             ts_no_ds = np.mean(ts_no_ds[~masked_chans, :], axis=0)
-            #scale the ts with the std so everythin is in units of noise
-            #FLUENCE = fit_FLUENCE(
-            #    ts_no_ds/std,
-            #    tsamp,
-            #    3 * sigma_width,
-            #    nsamp=int(loc / tsamp),
-            #    ds_data=waterfall_dat,
-            #    plot=False,
-            #)
         else:
             FLUENCE = -1
     else:
         # fit using downsampled values
         # this is mostly used for the injections
         try:
+            # plt.plot(dat_ts)
+            # plt.show()
             amp, std, loc, sigma_width = autofit_pulse(
                 dat_ts,
                 tsamp * downsamp,
@@ -238,10 +215,10 @@ def grab_spectra_manual(
         std = std * np.sqrt(sum(~masked_chans)/len(masked_chans))
         SNR = amp / std
         # because loc is predetermined set start and end a predifined spot
-        ts_no_ds_zoom_start = int(4.1/tsamp)
-        ts_no_ds_zoom_end = int(5.9/tsamp)
-        ts_no_ds = data[:, ts_no_ds_zoom_start : ts_no_ds_zoom_end]
-        ts_no_ds = np.mean(ts_no_ds[~masked_chans, :], axis=0)
+        # ts_no_ds_zoom_start = int(4.1/tsamp)
+        # ts_no_ds_zoom_end = int(5.9/tsamp)
+        # ts_no_ds = data[:, ts_no_ds_zoom_start : ts_no_ds_zoom_end]
+        # ts_no_ds = np.mean(ts_no_ds[~masked_chans, :], axis=0)
         #FLUENCE = fit_FLUENCE(
         #    ts_no_ds/std,
         #    tsamp,
@@ -280,12 +257,12 @@ def find_polynomial_fit(x_std, ts_std, order = None):
         # find the minimum rchi2
         rchi2_arr = (np.array(rchi2_arr)-1)**2
         ind = np.argmin(rchi2_arr)
-        print(std_arr)
+        # print(std_arr)
         std_diff = np.abs(np.diff(std_arr))
-        print(std_diff)
+        # print(std_diff)
         #find where std_diff first is smaller than 0.0003
         ind_std = np.where(std_diff < 0.0002)[0][0]
-        print(ind_std)
+        # print(ind_std)
         poly = poly_arr[ind_std+1]
         coeffs = coeffs_arr[ind_std+1]
     else:
@@ -306,6 +283,7 @@ def autofit_pulse(ts, tsamp, width, nsamps, ds_data, downsamp, plot=True, plot_n
     x_std = np.delete(x, range(int(ind_max - w_bin), int(ind_max + w_bin)))
     # ts_std = ts
     poly, coeffs = find_polynomial_fit(x_std, ts_std)
+    print(len(coeffs),coeffs)
     # subtract the mean
     ts_sub = ts - poly(x)
     ts_std_sub = ts_std - poly(x_std)
@@ -324,6 +302,7 @@ def autofit_pulse(ts, tsamp, width, nsamps, ds_data, downsamp, plot=True, plot_n
         method="Nelder-Mead",
     )
     fitx = max_l.x
+    print(fitx)
     fitx[0] = abs(fitx[0])
     fitx[1] = abs(fitx[1])
     fitx[2] = abs(fitx[2])
@@ -600,7 +579,6 @@ class inject_obj:
             t_dur = (period-0.1)*2
             t_start = 5-(t_dur/2)
             fit_del = t_dur*0.055
-
         fluence, std, amp, gaussian_amp, sigma_width, det_snr, approximate_toa = grab_spectra_manual(
             gf=self.filfile,
             ts=ts,

statistics_scripts_with_SNR_uncertainty/dynesty_plot.py
@@ -136,22 +136,38 @@ class dynesty_plot:
         stds = []
         for f,d in zip(self.filename,self.data):
             if plot:
-                corner = dyplot.cornerplot(d.results, labels=labels)
+                corner = dyplot.cornerplot(d.results, labels=labels,quantiles=[0.025,0.16,0.5,0.84,0.975],verbose=True)
                 if (period > 0) & (obs_time > 0):
                     #if these two global variables are set
                     try:
                         N_ax = corner[1][2][2]
                         N_sax = N_ax.secondary_xaxis('top', functions=(Ntonull,nulltoN))
+                        #place a vertical line
+                        Nbursts = nulltoN(0.41)
+                        N_ax.axvline(Nbursts,color='r',linestyle='--')
+                        # Nbursts = nulltoN(0)
+                        #grey out ng et al for J2044
+                        # Nbursts_null_ng = nulltoN(0.09)
+                        # xlims = N_ax.get_xlim()
+                        # ylims = N_ax.get_ylim()
+                        # N_ax.fill_between([xlims[0],Nbursts_null_ng],ylims[1],ylims[0],color='red',alpha=0.2)
+                        #grew out upper regions
+                        # xlims = N_ax.get_xlim()
+                        # ylims = N_ax.get_ylim()
+                        # N_ax.fill_between([Nbursts,xlims[1]],ylims[0],ylims[1],color='grey',alpha=0.8)
+
                         N_sax.set_xlabel(r"Nulling fraction")
                     except:
                         N_ax = corner[1][1][1]
                         N_sax = N_ax.secondary_xaxis('top', functions=(Ntonull,nulltoN))
                         N_sax.set_xlabel(r"Nulling fraction")
                 plt.savefig(f.strip(".h5")+"_corner.pdf")
-
                 plt.savefig(f.strip(".h5")+"_corner.png")
                 plt.close()
-
+            #import quantiles
+            from dynesty.utils import quantile as _quantile
+            samples = d.results['samples']
+            weights = d.results.importance_weights()
             mean,cov = dynesty.utils.mean_and_cov(d.results.samples,d.results.importance_weights())
             diag_cov = np.diag(cov)
             std = np.sqrt(diag_cov)
@@ -290,6 +306,7 @@ class dynesty_plot:
             #load the detections
             det_fluence, det_width, det_snr, noise_std = process_detection_results(real_det)
             det_snr = det_snr[det_snr > snr_thresh]
+            print("# dets",len(det_snr))
             fit_x = np.linspace(1e-9, np.max(det_snr)*2, 10000)
             k = m[0]
             det_error = statistics_exp.det_error
@@ -297,7 +314,7 @@ class dynesty_plot:
             # fit_y = smoothTriangle(fit_y, 100)
             fit_y = fit_y / np.trapz(fit_y, fit_x)
             fig, ax = plt.subplots(1, 1)
-            ax.hist(det_snr, bins='auto', density=True, label=r"$S_{det}$")
+            ax.hist(det_snr, bins='auto', density=True, label=r"$S_{det}$",alpha=0.5)
             ax.plot(fit_x, fit_y, label="fit",linewidth=2)
             ax.set_xlim(-(np.max(det_snr)*0.01), np.max(det_snr)*1.2)
             ax.set_ylim(0, 1.2*np.max(fit_y))
@@ -310,6 +327,7 @@ class dynesty_plot:
             ax2.set_xlabel(r"$S_{det}$")
             ax2.set_ylabel(r"P(det|$S_{det}$)")
             ax.set_ylabel("Probability density")
+            ax.set_xlabel("S/N")
             ax.legend(loc=1)
             ax2.legend(loc=2)
             plt.savefig(f"{f}_exp_fit.pdf")
@@ -337,6 +355,7 @@ class dynesty_plot:
             #load the detections
             det_fluence, det_width, det_snr, noise_std = process_detection_results(real_det)
             det_snr = det_snr[det_snr > snr_thresh]
+            print("# dets",len(det_snr))
             fit_x = np.linspace(1e-9, np.max(det_snr)*2, 10000)
             mu = m[0]
             std = m[1]
@@ -345,7 +364,7 @@ class dynesty_plot:
             # fit_y = smoothTriangle(fit_y, 100)
             fit_y = fit_y / np.trapz(fit_y, fit_x)
             fig, ax = plt.subplots(1, 1)
-            ax.hist(det_snr, bins='auto', density=True, label=r"$S_{det}$")
+            ax.hist(det_snr, bins='auto', density=True, label=r"$S_{det}$",alpha=0.5)
             ax.plot(fit_x, fit_y, label="fit",linewidth=2)
             ax.set_xlim(-(np.max(det_snr)*0.01), np.max(det_snr)*1.2)
             ax.set_ylim(0, 1.2*np.max(fit_y))
@@ -355,11 +374,12 @@ class dynesty_plot:
             ax2 = ax.twinx()
             ax2.set_ylim(0, 1.01)
             ax2.plot(conv_amp_array, p_det, label=r"P(det|$S_{det}$)", color='b',linewidth=2,alpha=0.6)
-            ax2.set_xlabel("SNR")
+            ax.set_xlabel("S/N")
             ax2.set_ylabel(r"P(det|$S_{det}$)")
             ax.set_ylabel("Probability density")
             ax.legend(loc=1)
             ax2.legend(loc=2)
+            # ax.set_xlim(0,30)
             plt.savefig(f"{f}_logn_fit.pdf")
             plt.savefig(f"{f}_logn_fit.png")
             # plt.show()
@@ -393,8 +413,8 @@ if __name__=="__main__":
             dp.plot_bayes_ratio_exp()
 
     else:
-        dp.plot_corner(labels=[r"$\mu$",r"$\sigma$","N"],plot=False)
-        # dp.plot_fit_logn()
+        dp.plot_corner(labels=[r"$\mu$",r"$\sigma$","N"],plot=True)
+        dp.plot_fit_logn()
         if plot_accuracy:
             dp.plot_accuracy_logn()
         if args.bayes_ratio: