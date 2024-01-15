e1cbdc68b21b051da95617381374ebe03db4a556
adam <adamdong@phas.ubc.ca>
2024-01-15 09:33:48 -0800

    some local changes

--- injection_scripts_fluence/batch_extract_snr.sh
+++ injection_scripts_fluence/batch_extract_snr.sh
@@ -10,4 +10,5 @@
 #
 source ~/util/load_presto.sh
 source ~/rrat_or_not_width/bin/activate
-python /home/adamdong/rrat_or_not_with_width/rrat_or_not/injection_scripts_fluence/extract_snr.py -dm $1 -o $2 -ds 3 -p $3 -cutout 0.8 >> extract_snr.log
+echo "python /home/adamdong/rrat_or_not_with_width/rrat_or_not/injection_scripts_fluence/extract_snr.py -dm $1 -o $2 -ds 3 -p $3 -cutout 0.8 -period $4" >> extract_command.txt
+python /home/adamdong/rrat_or_not_with_width/rrat_or_not/injection_scripts_fluence/extract_snr.py -dm $1 -o $2 -ds 3 -p $3 -cutout 0.8 -period $4 >> extract_snr.log
