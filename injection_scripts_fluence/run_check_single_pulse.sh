832ab294c15d91e9e085fc64feec55917972148e
Your Name <you@example.com>
2024-03-10 12:09:00 -0700

    adding check for rfifind files

--- injection_scripts_fluence/run_check_single_pulse.sh
+++ injection_scripts_fluence/run_check_single_pulse.sh
@@ -21,6 +21,28 @@ do
     echo $FOL
     cd $FOL
     #copy the filterbank file back in
+    #check how many rfifind files there are in the directory
+    inject_files=($(ls $FOL/*SNR*.fil))
+    #loop over all inject files
+    for inject in $inject_files
+    do
+        #strip the extension
+        inject_fol="${inject%.*}"
+        #check that inject_fol exists
+        if [ -d "$inject_fol" ]; then
+            rfifind_files=$(ls inject_fol/*rfifind* | wc -l)
+            #if less than 5 then remove the directory and continue
+            if [ $rfifind_files -lt 5 ]; then
+                echo "not enough rfifind files, removing directory"
+                rm -r inject_fol
+            fi
+        fi
+
+    done
+    read var1
+    if [ $rfifind_files -lt 5 ]; then
+        echo "Not enough rfifind files, skipping"
+    fi
     if [ "$LOCAL" != true ]; then
         "$SCRIPT_DIR"/check_single_pulse.sh -b -d $dm *SNR*.fil
         "$SCRIPT_DIR"/check_single_pulse.sh -f *SNR*.fil
