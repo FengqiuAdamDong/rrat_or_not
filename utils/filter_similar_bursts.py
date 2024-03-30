abc005c79219e986582912a20e9ca5e807ade20c
adamdong <adamdong@phas.ubc.ca>
2024-03-30 07:50:19 -0700

    bug fix

--- utils/filter_similar_bursts.py
+++ utils/filter_similar_bursts.py
@@ -8,7 +8,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-csv_path" ,nargs='+' , help="path to folder containing the files")
 parser.add_argument("-dm", help="dm of the burst",type=float)
-parser.add_argument("-copy", help="copy",type=bool,action="store_true")
+parser.add_argument("-copy", help="copy",action="store_true")
 args = parser.parse_args()
 csv_path = args.csv_path
 copy = args.copy
