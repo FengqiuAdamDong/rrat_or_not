7611e9c7408e5623530ee6341ced9350716e2b6e
adamdong <adamdong@phas.ubc.ca>
2024-04-02 13:23:04 -0700

    removed the copy argument

--- injection_scripts_fluence/extract_pipeline.py
+++ injection_scripts_fluence/extract_pipeline.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
             print(f"python {filter_script_path} -csv_path positive_bursts_1.csv -dm {dm} -copy")
             # print(f"python {create_positive_csv_edit_path} filtered.csv")
             try:
-                subprocess.run(["python", filter_script_path, "-csv_path", "positive_bursts_1.csv", "-dm", dm, "-copy"],check=True)
+                subprocess.run(["python", filter_script_path, "-csv_path", "positive_bursts_1.csv", "-dm", dm],check=True)
                 # subprocess.run([f"python", f"{filter_script_path}"],check=True)
                 # subprocess.run([f"python {create_positive_csv_edit_path} filtered.csv"],check=True)
             except Exception as e:
