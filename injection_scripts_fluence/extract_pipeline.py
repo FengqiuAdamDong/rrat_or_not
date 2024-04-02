8fc0137c13759ede5c005f019f0be089a5b426ca
adamdong <adamdong@phas.ubc.ca>
2024-04-02 13:23:41 -0700

    removed the print statement

--- injection_scripts_fluence/extract_pipeline.py
+++ injection_scripts_fluence/extract_pipeline.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
 
         if process_filter:
             #print command
-            print(f"python {filter_script_path} -csv_path positive_bursts_1.csv -dm {dm} -copy")
+            print(f"python {filter_script_path} -csv_path positive_bursts_1.csv -dm {dm}")
             # print(f"python {create_positive_csv_edit_path} filtered.csv")
             try:
                 subprocess.run(["python", filter_script_path, "-csv_path", "positive_bursts_1.csv", "-dm", dm],check=True)
