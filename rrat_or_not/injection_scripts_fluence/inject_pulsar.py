8fcb415b8023b589dd5d5f4d8ff332f8fe60bac3
fengqiu.dong@gmail.com <fengqiu.dong@gmail.com>
2025-12-26 14:40:13 -0500

    lower snr

--- rrat_or_not/injection_scripts_fluence/inject_pulsar.py
+++ rrat_or_not/injection_scripts_fluence/inject_pulsar.py
@@ -334,7 +334,7 @@ if __name__ == "__main__":
     dm = 1778
     epoch = 50000 # MJD
     pulse_width = 10e-3  # seconds
-    pulse_snr = 100
+    pulse_snr = 1
     downsamp = 1
     orbit_sampling_rate = 45 #days
 
